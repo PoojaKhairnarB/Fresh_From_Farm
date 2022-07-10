b3631b6629f999872ef6b353622c4532a5ad3ea2
PoojaKhairnarB <poojakhairnar.1995@gmail.com>
2022-07-10 16:44:20 +0100

    Forget Password

--- app/urls.py
+++ app/urls.py
@@ -26,14 +26,15 @@ urlpatterns = [
     path('dairy/', views.dairy, name='dairy'),
     path('dairy/ <slug:data>', views.dairy, name='dairydata'),
     
-   # path('login/', views.login, name='login'),
     path ('accounts/login/', auth_views.LoginView.as_view(template_name ='app/login.html', authentication_form=LoginForm), name='login'),
     
     path ('logout/', auth_views.LogoutView.as_view(next_page = 'login'), name='logout'),
 
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     
-    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm),name='passwordchange'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm , success_url ='/passwordchangedone/'),name='passwordchange'),
+    
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     
     path('checkout/', views.checkout, name='checkout'),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
