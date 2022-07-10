fdd4c34bac51c068ef4fca8d6b0bec3b49ed1629
PoojaKhairnarB <poojakhairnar.1995@gmail.com>
2022-07-10 17:51:37 +0100

    Email sent - Forget password

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm, MyPasswordChangeForm
+from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm , MySetPasswordForm
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home') ,
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
@@ -36,5 +36,15 @@ urlpatterns = [
     
     path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
     
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
+
     path('checkout/', views.checkout, name='checkout'),
+    
+    
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
